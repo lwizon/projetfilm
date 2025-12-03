f31d192beacbc25f38cbf1c624d7e6fc4fbf79e2
qnoqno <117929371+qnoqno@users.noreply.github.com>
2025-12-03 09:44:48 +0100

    debug

--- ProjetFilmv1/obj/Debug/net9.0-windows/ProjetFilmv1.AssemblyInfo.cs
+++ ProjetFilmv1/obj/Debug/net9.0-windows/ProjetFilmv1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProjetFilmv1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea562d113d979c48315c8b52f6c5d42e5709702")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+096629460ba70023dea08f2b14f8dfff70425e17")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProjetFilmv1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProjetFilmv1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
