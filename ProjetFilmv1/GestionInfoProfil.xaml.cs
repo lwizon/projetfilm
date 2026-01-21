f9b603720e3a3c17d581ca2d66ca7b63cab93830
Akii888 <eloigirardet10@gmail.com>
2026-01-21 09:42:39 +0100

    test

---
+++ ProjetFilmv1/GestionInfoProfil.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+
+namespace ProjetFilmV1 
+{
+    public partial class GestionInfoProfil_xaml : Page 
+    {
+        public GestionInfoProfil_xaml()
+        {
+
+            InitializeComponent(); 
+        }
+        
+    }
+}
