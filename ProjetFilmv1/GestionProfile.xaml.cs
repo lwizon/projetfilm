5fe4c155a7ab36e5429a7ca3cfdb56752022aeed
Akii888 <eloigirardet10@gmail.com>
2026-01-21 09:42:39 +0100

    test

---
+++ ProjetFilmv1/GestionProfile.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using ProjetFilmv1;
+using ProjetFilmV1;
+
+
+namespace ProfileApp 
+{
+    public partial class MainWindow : Window 
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        
+        private void OpenProfileDetails(object sender, RoutedEventArgs e)
+        {
+            HeaderImg.Visibility = Visibility.Collapsed;
+            
+            MenuStackPanel.Visibility = Visibility.Collapsed;
+            
+            MainFrame.Visibility = Visibility.Visible;
+            
+            MainFrame.Navigate(new GestionInfoProfil_xaml());
+        }
+    }
+}
